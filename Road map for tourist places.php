a095b48569d7055b10518b9e5ccb89642dc4be2d
Sam95sam <95samuvel@gmail.com>
2025-05-12 03:42:25 -0700

    Update

---
+++ Road map for tourist places.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title> Road Map For Tourist Places </title>
+<style>
+a {text-decoration:none;margin:20px;text-color:blue;color:black;border:2px;}
+h1 {border:none;color:white;background-color:blue;padding:15px;font-size:25px;}
+h2 {border:2px;color:black;background-color:yellow;padding:8px;font-size:22px;}
+</style>
+</head>
+<body>
+<form>
+<div class = "main" >
+<center>
+<h1>  Road Map For Tourist Places </h1>
+<h2><a href= "homepage2.html"> Home </a> &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+<a href="Places.html"> Places </a>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; <a href= "things.html" > Things </a>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 
+<a href="about.html"> About </a> </h2> 
+</center>
+
+<a href = " kodaikanal.html"> <img src = "10.png"   style = "width:30%;height:30%;"> </a>
+<a href ="munnar.html"> <img src = "8.png" style = "width:30%;height:30%;"> </a>
+<a href ="coorg.html"> <img src = "5.png" style = "width:30%;height:30%;"> </a><br><br><br>
+
+<a href = " chikmaglur.html"> <img src = "6.png"   style = "width:30%;height:30%;"> </a>
+<a href = " wayanad.html"> <img src = "7.png"   style = "width:30%;height:30%;"> </a>
+<a href ="ooty.html"> <img src = "9.png" style = "width:30%;height:30%;"> </a><br><br><br>
+
+<a href ="kanyakumari.html"> <img src = "kk14.png" style = "width:30%;height:30%;"> </a>
+<a href ="thanjavur.html"> <img src = "thanjavur.png" style = "width:30%;height:30%;"> </a>
+<a href ="chennai.html"> <img src = "15.png" style = "width:30%;height:30%;"> </a><br><br><br>
+
+
+<a href ="kochi.html"> <img src = "kochi.png" style = "width:30%;height:30%;"> </a>
+<a href ="goa.html"><img src = "17.png" style = "width:30%;height:30%;"> </a>
+<a href ="bangalur.html"> <img src = "16.png" style = "width:30%;height:30%;">  </a><br><br><br>
+
+</form>
+</div>
+</body>
+</html>
